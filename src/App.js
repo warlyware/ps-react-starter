26cbc40a1c147b06381904db07134a2cf4ff7dcf
Dan Ward <daniel.ward@forrent.com>
2017-09-19 16:29:13 -0400

    Use props and setState to create incremental counter on button click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const Button = () => {
-  return(
-    <button>42</button>
-  );
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1
+    }));
+  }
+
+  render() {
+    return(
+      <button onClick={this.handleClick}>
+        {this.state.counter}
+      </button>
+    );
+  }
 }
 
 class App extends Component {

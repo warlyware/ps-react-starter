db1209979ded48abd800962c062ee9fffeb219e9
Dan Ward <daniel.ward@forrent.com>
2017-09-19 17:00:36 -0400

    Start github card project, add Card with placeholder content

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Button extends Component {
-
-  handleClick = () => {
-    this.props.handleClick(this.props.incrementValue);
-  }
-
-  render() {
-    return(
-      <button
-      onClick={this.handleClick}>
-        +{this.props.incrementValue}
-      </button>
-    );
-  }
-}
-
-const Result = (props) => {
+const Card = (props) => {
   return(
-    <div>{props.counter}</div>
-  );
+    <div>
+      <img src="//placehold.it/75" />
+      <div>
+        <div>Name here...</div>
+        <div>Company here...</div>
+      </div>
+    </div>
+  )
 }
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-  }
-
-  incrementCounter = (incrementValue) => {
-    this.setState((prevState) => ({
-      counter: prevState.counter + incrementValue
-    }));
-  }
 
   render() {
     return (
-      <div>
-        <Button incrementValue={1} handleClick={this.incrementCounter} />
-        <Button incrementValue={5} handleClick={this.incrementCounter} />
-        <Button incrementValue={10} handleClick={this.incrementCounter} />
-        <Button incrementValue={100} handleClick={this.incrementCounter} />
-        <Result counter={this.state.counter} />
-      </div>
+      <Card />
     );
   }
 }

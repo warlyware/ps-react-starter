7ed7a5b29d7250522b2269a92c5148d8af2932f2
Dan Ward <daniel.ward@forrent.com>
2017-09-22 11:13:46 -0400

    Add numbers to Number component

--- src/App.js
+++ src/App.js
@@ -36,15 +36,17 @@ const Answer = (props) => {
 }
 
 const Numbers = (props) => {
+
   return(
     <div className="well text-center">
-      <span>1</span>
-      <span className="selected">2</span>
-      <span className="used">3</span>
-      ...
+      {Numbers.list.map((number, i) =>
+        <span key={i}>{number}</span>
+      )}
     </div>
   );
 }
+
+Numbers.list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 class Game extends Component {
 
